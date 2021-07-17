acc1e0ec935d535fa803f61e3257f2688af38daa
waranat suttikarn <waranat.cpe@gmail.com>
2021-07-17 15:02:14 +0700

    feat: Add LIFF and Theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+import { ChakraProvider, useColorMode } from "@chakra-ui/react";
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+const liffId: string = process.env.NEXT_PUBLIC_LIFF_ID
+  ? process.env.NEXT_PUBLIC_LIFF_ID
+  : "";
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const getLiff = async () => {
+      const liff = (await import("@line/liff")).default;
+      try {
+        await liff.init({ liffId });
+      } catch (error) {
+        console.error("liff init error", error.message);
+      }
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      }
+    };
+    getLiff();
+  });
+
+  return (
+    <ChakraProvider>
+      <Navbar />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+export default App;
