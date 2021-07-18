c9c626c9d52af9a3a5f696f9536979e5afb70104
waranat suttikarn <waranat.cpe@gmail.com>
2021-07-18 23:11:49 +0700

    fix: enable liff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,26 +9,26 @@ import store from "../store";
 
 import router, { useRouter } from "next/router";
 
-// const liffId: string = process.env.NEXT_PUBLIC_LIFF_ID
-//   ? process.env.NEXT_PUBLIC_LIFF_ID
-//   : "";
+const liffId: string = process.env.NEXT_PUBLIC_LIFF_ID
+  ? process.env.NEXT_PUBLIC_LIFF_ID
+  : "";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   const Router = useRouter();
-  // useEffect(() => {
-  //   const getLiff = async () => {
-  //     const liff = (await import("@line/liff")).default;
-  //     try {
-  //       await liff.init({ liffId });
-  //     } catch (error) {
-  //       console.error("liff init error", error.message);
-  //     }
-  //     if (!liff.isLoggedIn()) {
-  //       liff.login();
-  //     }
-  //   };
-  //   getLiff();
-  // });
+  useEffect(() => {
+    const getLiff = async () => {
+      const liff = (await import("@line/liff")).default;
+      try {
+        await liff.init({ liffId });
+      } catch (error) {
+        console.error("liff init error", error.message);
+      }
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      }
+    };
+    getLiff();
+  });
   if (Router.pathname === "/auth/signin") {
     return (
       <ChakraProvider>
